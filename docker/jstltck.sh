8666fc4cd701b4a356fc9009025f3bf9c93801c5
Alwin Joseph <44426046+alwin-joseph@users.noreply.github.com>
2021-03-03 13:00:30 +0530

    jstl set locale provider as COMPAT

--- docker/jstltck.sh
+++ docker/jstltck.sh
@@ -59,6 +59,7 @@ if [[ "$JDK" == "JDK11" || "$JDK" == "jdk11" ]];then
   export JAVA_HOME=${JDK11_HOME}
   export PATH=$JAVA_HOME/bin:$PATH
   cp ts.jte.jdk11 ts.jte
+  export JAVA_OPTIONS="-Djava.locale.providers=COMPAT"
 fi
 
 which java
